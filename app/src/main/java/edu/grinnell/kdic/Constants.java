a90510163edf561c4a7313b9accd476fc8cc8e53
nicarmani <jnic.armani@gmail.com>
2016-06-04 18:35:14 -0300

    Fix

--- app/src/main/java/edu/grinnell/kdic/Constants.java
+++ app/src/main/java/edu/grinnell/kdic/Constants.java
@@ -7,6 +7,9 @@ public abstract class Constants {
     public static final String ACTION_STREAM_PLAY_PAUSE = "edu.grinnell.kdic.action.PLAY_PAUSE";
     public static final String ACTION_STOP_RADIO_SERVICE = "edu.grinnell.kdic.action.STOP_RADIO";
 
+    public static final String SHARED_PREFS = "shared_prefs";
+    public static final String FIRST_RUN = "first_run";
+
 
     // for parsing JSON
     public static final String JSON_DAYS = "days";
