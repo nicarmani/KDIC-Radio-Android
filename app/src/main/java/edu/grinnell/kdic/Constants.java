838fb601b9c9047bcb75bbd65ee7177288190651
nicarmani <jnic.armani@gmail.com>
2016-06-04 18:05:21 -0300

    Update Constants.java

--- app/src/main/java/edu/grinnell/kdic/Constants.java
+++ app/src/main/java/edu/grinnell/kdic/Constants.java
@@ -4,8 +4,8 @@ public abstract class Constants {
     public static final String STREAM_URL = "http://kdic.grinnell.edu:8001/stream";
     public static final String SCHEDULE_URL = "http://kdic.grinnell.edu/scheduleScript.php";
 
-    public static final String ACTION_STREAM_PLAY_PAUSE = "edu.grinnell.kdic.action.PLAY_PAUSE";
-    public static final String ACTION_STOP_RADIO_SERVICE = "edu.grinnell.kdic.action.STOP_RADIO";
+    public static final String STREAM_URL = "http://streaming210.radionomy.com:80/WEBREGGAETON";
+    public static final String SCHEDULE_URL = "http://amistadfmvh.ml/test/scheduleScript.php";
 
     public static final String SHARED_PREFS = "shared_prefs";
     public static final String FIRST_RUN = "first_run";
